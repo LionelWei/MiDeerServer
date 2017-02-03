528564ffaf00dd4d646fcc791c95b2646387c1b6
LionelWei <lionel.wei@outlook.com>
2017-02-03 12:31:56 +0800

    add static res

--- mideer.js
+++ mideer.js
@@ -7,26 +7,26 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', {data: 'this is extra data'})
 })
 
 app.use((req, res) => {
-  res.type('text/html');
   res.status(404);
-  res.send('{"haha": "hello"}');
+  res.render('404');
 })
 
-app.use((err, req, res, next) => {
-  console.error('500 error stack: ');
-  console.error(JSON.stringify(err, null, 2));
-  res.type('text/plain');
-  res.status(500);
-  res.send('500 - Server Error');
-})
-
+// app.use((err, req, res, next) => {
+//   console.error('500 error stack: ');
+//   console.error(JSON.stringify(err, null, 2));
+//   res.type('text/plain');
+//   res.status(500);
+//   res.send('500 - Server Error');
+// })
+//
 app.listen(app.get('port'), () => {
   console.log('Express started on http://localhost: ' + app.get('port') + '; press Ctrl-C to terminate');
 })
